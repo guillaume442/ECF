6afb4f60310ca5844380d5e7628ff097a1f3624c
guillaume442 <gui.cab059@gmail.com>
2024-01-16 11:10:42 +0100

    5485

--- script.js
+++ script.js
@@ -1,125 +1,31 @@
-// Récupère le modal
-var modal = document.getElementById("modal1");
+// Fonction pour attacher les événements au modal et au bouton
+function attachModalEvents(modalId, btnId) {
+    var modal = document.getElementById(modalId);
+    var btn = document.getElementById(btnId);
+    var span = modal.querySelector('.close');
 
-// Récupère le bouton qui ouvre le modal
-var btn = document.getElementById("btn1");
+    // Lorsque l'utilisateur clique sur le bouton, ouvre le modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
 
-// Récupère l'élément <span> qui ferme le modal
-var span = document.getElementsByClassName("close")[0];
-
-// Lorsque l'utilisateur clique sur le bouton, ouvre le modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// Lorsque l'utilisateur clique sur <span> (x), ferme le modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// Lorsque l'utilisateur clique n'importe où en dehors du modal, le ferme
-window.onclick = function(event) {
-    if (event.target == modal) {
+    // Lorsque l'utilisateur clique sur <span> (x), ferme le modal
+    span.onclick = function() {
         modal.style.display = "none";
     }
-}
-// Récupère le modal
-var modal = document.getElementById("modal2");
 
-// Récupère le bouton qui ouvre le modal
-var btn = document.getElementById("btn2");
-
-// Récupère l'élément <span> qui ferme le modal
-var span = document.getElementsByClassName("close")[0];
-
-// Lorsque l'utilisateur clique sur le bouton, ouvre le modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// Lorsque l'utilisateur clique sur <span> (x), ferme le modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// Lorsque l'utilisateur clique n'importe où en dehors du modal, le ferme
+    // Lorsque l'utilisateur clique n'importe où en dehors du modal, le ferme
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-// Récupère le modal
-var modal = document.getElementById("modal3");
-
-// Récupère le bouton qui ouvre le modal
-var btn = document.getElementById("btn3");
-
-// Récupère l'élément <span> qui ferme le modal
-var span = document.getElementsByClassName("close")[0];
-
-// Lorsque l'utilisateur clique sur le bouton, ouvre le modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// Lorsque l'utilisateur clique sur <span> (x), ferme le modal
-span.onclick = function() {
     modal.style.display = "none";
-}
-
-// Lorsque l'utilisateur clique n'importe où en dehors du modal, le ferme
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
     }
-}
-// Récupère le modal
-var modal = document.getElementById("modal4");
-
-// Récupère le bouton qui ouvre le modal
-var btn = document.getElementById("btn4");
-
-// Récupère l'élément <span> qui ferme le modal
-var span = document.getElementsByClassName("close")[0];
-
-// Lorsque l'utilisateur clique sur le bouton, ouvre le modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// Lorsque l'utilisateur clique sur <span> (x), ferme le modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// Lorsque l'utilisateur clique n'importe où en dehors du modal, le ferme
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
     }
-}
-// Récupère le modal
-var modal = document.getElementById("modal5");
-
-// Récupère le bouton qui ouvre le modal
-var btn = document.getElementById("btn5");
-
-// Récupère l'élément <span> qui ferme le modal
-var span = document.getElementsByClassName("close")[0];
-
-// Lorsque l'utilisateur clique sur le bouton, ouvre le modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// Lorsque l'utilisateur clique sur <span> (x), ferme le modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// Lorsque l'utilisateur clique n'importe où en dehors du modal, le ferme
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
     }
-}
+    
+    // Attacher les événements à chaque modal et bouton
+    attachModalEvents("modal1", "btn1");
+    attachModalEvents("modal2", "btn2");
+    attachModalEvents("modal3", "btn3");
+    attachModalEvents("modal4", "btn4");
+    attachModalEvents("modal5", "btn5");
+
